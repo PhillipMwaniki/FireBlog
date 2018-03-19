9f0e438653cf7c95cb8c2b1ddb1a0e827945d7bd
Phillip Mwaniki Nzuli <mwanikiphillip@gmail.com>
2018-03-19 13:09:11 +0300

    added reader-module to app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { EditorModule } from './editor-module/editor.module';
+import { ReaderModule } from './reader-module/reader.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, EditorModule],
+    imports: [BrowserModule, EditorModule, ReaderModule],
     providers: [],
     bootstrap: [AppComponent]
 })
---
+++ src/app/reader-module/reader.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReaderPostComponent } from './components/reader-post/reader-post.component';
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [ReaderPostComponent]
+})
+export class ReaderModule {}

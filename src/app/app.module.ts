ff6cfe02b727210418cd6e8afc94c73b5f0059c6
Phillip Mwaniki Nzuli <mwanikiphillip@gmail.com>
2018-03-19 13:14:57 +0300

    Added page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 
 import { EditorModule } from './editor-module/editor.module';
 import { ReaderModule } from './reader-module/reader.module';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, PageNotFoundComponent],
     imports: [BrowserModule, EditorModule, ReaderModule],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/reader-module/reader.module.ts
+++ src/app/reader-module/reader.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReaderPostComponent } from './components/reader-post/reader-post.component';
 import { ReaderPostsComponent } from './components/reader-posts/reader-posts.component';
+import { ReaderRoutingModule } from './reader-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [CommonModule],
-    declarations: [ReaderPostComponent, ReaderPostsComponent]
+    imports: [CommonModule, FormsModule, ReaderRoutingModule],
+    declarations: [ReaderPostComponent, ReaderPostsComponent],
+    exports: [],
+    providers: []
 })
 export class ReaderModule {}

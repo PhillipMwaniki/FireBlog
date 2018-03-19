910b3122d7d3a2e9f813a327b5b260608ba7a5d2
Phillip Mwaniki Nzuli <mwanikiphillip@gmail.com>
2018-03-19 13:04:52 +0300

    add routing-module to editor-module

--- src/app/editor-module/editor.module.ts
+++ src/app/editor-module/editor.module.ts
@@ -1,10 +1,14 @@
+import { EditorRoutingModule } from './editor-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EditorPostsComponent } from './editor-posts/editor-posts.component';
-import { EditorPostComponent } from './editor-post/editor-post.component';
+import { FormsModule } from '@angular/forms';
+import { EditorPostsComponent } from './components/editor-posts/editor-posts.component';
+import { EditorPostComponent } from './components/editor-post/editor-post.component';
 
 @NgModule({
-    imports: [CommonModule],
-    declarations: [EditorPostsComponent, EditorPostComponent]
+    imports: [CommonModule, FormsModule, EditorRoutingModule],
+    declarations: [EditorPostsComponent, EditorPostComponent],
+    exports: [],
+    providers: []
 })
 export class EditorModule {}

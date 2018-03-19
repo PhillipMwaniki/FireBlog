bd68245ac516b6d26a2963876fe3dac595b9476c
Phillip Mwaniki Nzuli <mwanikiphillip@gmail.com>
2018-03-19 13:10:35 +0300

    created read-posts components

--- src/app/reader-module/reader.module.ts
+++ src/app/reader-module/reader.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReaderPostComponent } from './components/reader-post/reader-post.component';
+import { ReaderPostsComponent } from './components/reader-posts/reader-posts.component';
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [ReaderPostComponent]
+    declarations: [ReaderPostComponent, ReaderPostsComponent]
 })
 export class ReaderModule {}
